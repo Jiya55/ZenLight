be355473d0ad50c8752a14b8d434ae9eb51890ce
jiya55 <jiyakirori@gmail.com>
2025-10-20 16:49:44 +0530

    candle decorator V1.0.0

--- code.js
+++ code.js
@@ -1,45 +1,105 @@
 var state = {
-    i:0,
-    j:0,
-    k:0,
+    candle_type:1,
+    cnd_color:0,
+    pattern:0,
 }
-
-function nextcolor(){
-    var color = document.getElementById("color_cnd");
-    if(state.i===0){
-        color.setAttribute("class", "color_cnd_1");
-        state.i++;
-    }else if(state.i===1){
-        color.setAttribute("class", "color_cnd_2");
-        state.i++;
-    }else if(state.i===2){
-        color.setAttribute("class", "color_cnd_3");
-        state.i++;
-    }
-}
-function nextcnd(){
+function set_shape(id) {
     var candle = document.getElementById("candle");
     var flame = document.getElementById("flame");
-    if(state.j===0){
+    if(id==1){
         candle.setAttribute("class", "shape_1");
         flame.setAttribute("class", "light_1");
-        state.j++;
-    }else if(state.j===1){
+        state.candle_type=1;
+    }
+    if(id==2){
         candle.setAttribute("class", "shape_2");
         flame.setAttribute("class", "light_2");
-        state.j++;
+        state.candle_type=2;
     }
 }
-function nextptn(){
+function set_color(id) {
+    var color = document.getElementById("color_cnd");
+    if (state.candle_type===1){
+        if (id==1){
+            color.setAttribute("class", "color_cnd_1");
+            state.cnd_color = 1;
+        }
+        if (id==2){
+            color.setAttribute("class", "color_cnd_2");
+            state.cnd_color = 2;
+        }
+        if (id==3){
+            color.setAttribute("class", "color_cnd_3");
+            state.cnd_color = 3;
+        }
+        if (id==4){
+            color.setAttribute("class", "color_cnd_4");
+            state.cnd_color = 4;
+        }
+        if (id==5){
+            color.setAttribute("class", "color_cnd_5");
+            state.cnd_color = 5;
+        }
+        if (id==6){
+            color.setAttribute("class", "color_cnd_6");
+            state.cnd_color = 6;
+        }
+    }
+    if (state.candle_type===2){
+        if (id==1){
+            color.setAttribute("class", "shp_2_color_cnd_1");
+            state.cnd_color = 1;
+        }
+        if (id==2){
+            color.setAttribute("class", "shp_2_color_cnd_2");
+            state.cnd_color = 2;
+        }
+        if (id==3){
+            color.setAttribute("class", "shp_2_color_cnd_3");
+            state.cnd_color = 3;
+        }
+        if (id==4){
+            color.setAttribute("class", "shp_2_color_cnd_4");
+            state.cnd_color = 4;
+        }
+        if (id==5){
+            color.setAttribute("class", "shp_2_color_cnd_5");
+            state.cnd_color = 5;
+        }
+        if (id==6){
+            color.setAttribute("class", "shp_2_color_cnd_6");
+            state.cnd_color = 6;
+        }
+    }
+}
+function set_pattern(id) {
     var ptn = document.getElementById("ptn_cnd");
-    if(state.k===0){
+    if (state.candle_type===1) {
+        if (id==1){
         ptn.setAttribute("class", "ptn_cnd_1");
-        state.k++;
-    }else if(state.k===1){
-        color.setAttribute("class", "ptn_cnd_2");
-        state.k++;
-    }else if(state.k===2){
-        color.setAttribute("class", "ptn_cnd_3");
-        state.k++;
+        state.pattern = 1;
+        }
+        if (id==2){
+            ptn.setAttribute("class", "ptn_cnd_2");
+            state.pattern = 2;
+        }
+        if (id==3){
+            ptn.setAttribute("class", "ptn_cnd_3");
+            state.pattern = 3;
+        }
+    }
+    if (state.candle_type===2) {
+        if (id==1){
+        ptn.setAttribute("class", "shp_2_ptn_cnd_1");
+        state.pattern = 1;
+        }
+        if (id==2){
+            ptn.setAttribute("class", "shp_2_ptn_cnd_2");
+            state.pattern = 2;
+        }
+        if (id==3){
+            ptn.setAttribute("class", "shp_2_ptn_cnd_3");
+            state.pattern = 3;
+        }
     }
 }
